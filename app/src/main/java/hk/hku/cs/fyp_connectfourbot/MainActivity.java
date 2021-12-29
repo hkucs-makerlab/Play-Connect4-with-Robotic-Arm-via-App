d19da30e1fa94b634fb578e2923adf86b219db2e
Alan Wan <alan2000909@gmail.com>
2021-12-30 01:47:48 +0800

    [Alan] Added OpenCV LIBRARY_PACKAGE 3.4.16, tested work

--- app/src/main/java/hk/hku/cs/fyp_connectfourbot/MainActivity.java
+++ app/src/main/java/hk/hku/cs/fyp_connectfourbot/MainActivity.java
@@ -3,8 +3,21 @@ package hk.hku.cs.fyp_connectfourbot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+
+    static {
+        if (OpenCVLoader.initDebug()){
+            Log.d(TAG, "Config successful");
+        }
+        else {
+            Log.d(TAG, "GG");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
