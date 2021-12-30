09754e49dfce459e8bf489660b708f07ed8d62c1
Alan Wan <alan2000909@gmail.com>
2021-12-30 23:30:23 +0800

    [Alan] Camera module accessed successfully

--- app/src/main/java/hk/hku/cs/fyp_connectfourbot/MainActivity.java
+++ app/src/main/java/hk/hku/cs/fyp_connectfourbot/MainActivity.java
@@ -4,18 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.InstallCallbackInterface;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
     private static String TAG = "MainActivity";
+    JavaCameraView javaCameraView;
+    Mat mRGBA, mRGBAT;
+
+
+    BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback() {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS:{
+                    javaCameraView.enableView();
+                    break;
+                }
+                default: {
+                    super.onManagerConnected(status);
+                }
+            }
+
+        }
+
+        @Override
+        public void onPackageInstall(int operation, InstallCallbackInterface callback) {
+            super.onPackageInstall(operation, callback);
+        }
+    };
 
     static {
         if (OpenCVLoader.initDebug()){
-            Log.d(TAG, "Config successful");
+            Log.d(TAG, "OpenCV Config successful 1");
         }
         else {
-            Log.d(TAG, "GG");
+            Log.d(TAG, "OpenCV Config failed");
         }
     }
 
@@ -23,5 +57,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        javaCameraView = (JavaCameraView) findViewById(R.id.camera_view);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(MainActivity.this);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        //Get the width and height to be displayed on the screen
+        Log.d(TAG, "Width: "+Integer.toString(width));
+        Log.d(TAG, "Height: "+Integer.toString(height));
+        mRGBA = new Mat(height, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        //Actions when the camera stop
+        mRGBA.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRGBA = inputFrame.rgba();
+        mRGBAT = mRGBA.t();
+        Core.flip(mRGBA.t(), mRGBAT, 1);
+        Imgproc.resize(mRGBAT, mRGBAT, mRGBA.size()); //resize the image (src, dst, size)
+        return mRGBAT;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (javaCameraView != null){
+            javaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV Config successful");
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        }
+        else {
+            Log.d(TAG, "OpenCV Config failed");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback);
+        }
     }
 }
