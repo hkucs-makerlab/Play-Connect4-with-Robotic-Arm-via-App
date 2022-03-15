bff7bd442d6c8f336c2b7d132cf936c1e871bde9
Alan Wan <alan2000909@gmail.com>
2022-03-15 14:37:13 +0800

    [Alan] validate function complete

--- app/src/main/java/hk/hku/cs/fyp_connectfourbot/BoardActivity.java
+++ app/src/main/java/hk/hku/cs/fyp_connectfourbot/BoardActivity.java
@@ -50,15 +50,21 @@ public class BoardActivity extends AppCompatActivity implements CameraBridgeView
     TextView textView;
     Button changeText;
     Mat mRGBA, mRGBAT;
-    public ArrayList<ArrayList<ArrayList<Integer>>> trueBoard = new ArrayList<>();
-    public int count = 10;
+    public int count = 50;
     public String realStr = "";
-    private Timer autoUpdate;
+
+    public ArrayList<ArrayList<ArrayList<Integer>>> trueBoard = new ArrayList<>();
     public ArrayList<ArrayList<Integer>> preState = new ArrayList<>();
     public ArrayList<ArrayList<Integer>> currentState = new ArrayList<>();
+    public ArrayList<Integer> validPos = new ArrayList<>();
+
+    public boolean initialReading = false;
 
     private CppActivity mCppActivity = new CppActivity();
     private TextView mTextView;
+    public String sequence = "";
+    public String firstPlayer = "";
+
 
     BroadcastReceiver FinishReceiver = new BroadcastReceiver() {
         @Override
@@ -114,6 +120,18 @@ public class BoardActivity extends AppCompatActivity implements CameraBridgeView
         mCppActivity.setNativeAssetManager(getAssets());
         mTextView = findViewById(R.id.score);
         textView = findViewById(R.id.textView2);
+        changeText = findViewById(R.id.update);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        javaCameraView = (JavaCameraView) findViewById(R.id.cameraview1);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(BoardActivity.this);
+        preState = initBoardState();
+        currentState = initBoardState();
+        Coordinates coor = (Coordinates) getIntent().getSerializableExtra("boardCoor");
+        if(coor != null){
+            trueBoard = coor.coor;
+            Log.d(TAG, "Data retrieved");
+        }
         Thread t = new Thread() {
 
             @Override
@@ -124,8 +142,8 @@ public class BoardActivity extends AppCompatActivity implements CameraBridgeView
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                validator();
                                 updateText();
+                                getBestMove();
                             }
                         });
                     }
@@ -135,17 +153,6 @@ public class BoardActivity extends AppCompatActivity implements CameraBridgeView
         };
 
         t.start();
-
-        changeText = findViewById(R.id.update);
-        localBroadcastManager = LocalBroadcastManager.getInstance(this);
-        javaCameraView = (JavaCameraView) findViewById(R.id.cameraview1);
-        javaCameraView.setVisibility(SurfaceView.VISIBLE);
-        javaCameraView.setCvCameraViewListener(BoardActivity.this);
-        Coordinates coor = (Coordinates) getIntent().getSerializableExtra("boardCoor");
-        if(coor != null){
-            trueBoard = coor.coor;
-            Log.d(TAG, "Data retrieved");
-        }
     }
 
 
@@ -155,14 +162,16 @@ public class BoardActivity extends AppCompatActivity implements CameraBridgeView
         Mat input = inputFrame.rgba();
 //        Log.d(TAG, "You're in board");
         Imgproc.cvtColor(input, input, Imgproc.COLOR_BGR2HSV);
+
+
         if (count != 0){
 //            Log.i(TAG, String.valueOf("Count:"+count));
             count--;
 
         }
         else{
-            count = 10;
-            Log.i(TAG, String.valueOf("--------------------------------S-------------------------------------"));
+            count = 50;
+//            Log.i(TAG, String.valueOf("--------------------------------S-------------------------------------"));
 
             String boardStr = "Board State: \n";
             for (int x=0; x < trueBoard.size() ; x++ ) {
@@ -172,7 +181,11 @@ public class BoardActivity extends AppCompatActivity implements CameraBridgeView
 //                    Imgproc.circle(input, p, 0, new Scalar(255, 0, 0), 5);
                     double[] pixel = input.get(trueBoard.get(x).get(y).get(1), trueBoard.get(x).get(y).get(0));
                     rowStr = getColor(pixel) + " " +rowStr;
-                    constructBoardState(getColor(pixel), x, y);
+                    if(!initialReading){
+                        constructBoardState(getColor(pixel), x, y, 0);
+                    }
+                    constructBoardState(getColor(pixel), x, y, 1);
+
 //                    rowStr = getColor(pixel) +"("+pixel[0]+ ","+ pixel[1]+ ","+ pixel[2]+")"+ " " +rowStr;
 //                    Log.i(TAG, String.valueOf("RGB: "+ getColor(pixel)));
 //                    Log.i(TAG, String.valueOf("Pixel: "+ pixel[0]+ ","+ pixel[1]+ ","+ pixel[2]));
@@ -190,10 +203,17 @@ public class BoardActivity extends AppCompatActivity implements CameraBridgeView
                 boardStr += rowStr + "\n";
 //                Log.i(TAG, String.valueOf("--------------------------------R"+x+"-------------------------------------"));
             }
+            if(!initialReading){
+                getInitValidPos();
+            }
             realStr = boardStr;
+
 //            Log.i(TAG, boardStr);
 //            textView.setText(boardStr);
-            Log.i(TAG, String.valueOf("--------------------------------End-------------------------------------"));
+//            Log.i(TAG, String.valueOf("--------------------------------End-------------------------------------"));
+            initialReading = true;
+            getSequence();
+
         }
         return inputFrame.rgba();
     }
@@ -212,27 +232,109 @@ public class BoardActivity extends AppCompatActivity implements CameraBridgeView
 
     }
 
+    public void displayBoard(){
+        //display the board state in the virtual board
+    }
+
+    public void displayHints(){
+
+    }
+
     public void changeText(View view){
-        String temp = "4453";
-        mTextView.setText(mCppActivity.mystringFromJNI(temp));
+//        Log.d(TAG, "Sequence: "+sequence);
+        if (sequence != ""){
+            mTextView.setText(mCppActivity.mystringFromJNI(sequence));
+        }
+    }
+
+    public void getBestMove(){
+        Log.d(TAG, "Sequence: "+sequence);
+        if (sequence != ""){
+            mTextView.setText(mCppActivity.mystringFromJNI(sequence));
+        }
     }
 
     public void updateText(){
         textView.setText(realStr);
     }
 
-    public void validator(){
-        Log.d(TAG, "HI");
+    public void getSequence(){
+        //compare the current board state and previous board state to output the sequence
+        ArrayList<Integer> tempCol = new ArrayList<>();
+        for ( int col = 0; col < 7; col++){
+            for (int row = 0; row < 6; row++){
+                if (currentState.get(col).get(row) - preState.get(col).get(row) > 0 && validate(col, row)){
+                    tempCol = preState.get(col);
+                    tempCol.set(row, currentState.get(col).get(row));
+                    preState.set(col, tempCol);
+                    sequence += String.valueOf(6-col+1);
+                }
+            }
+        }
     }
 
-    public void constructBoardState(String color, int x, int y){
+    public void getInitValidPos(){
+        //check the board to know the valid positions
+        for ( int col = 0; col < 7; col++){
+            boolean posConfirm = false;
+            for (int row = 0; row < 6; row++){
+
+                if(!posConfirm && preState.get(col).get(row)==0){
+                    Log.i(TAG, String.valueOf("pos:"+col+","+row));
+                    validPos.add(row);
+                    posConfirm = true;
+                }
+            }
+        }
+    }
+
+    public boolean validate(int col, int row){
+        Log.d(TAG, "row: "+row+"real row:"+String.valueOf(currentState.get(col).get(row)));
+        if(validPos.get(col) == row){
+            Log.i(TAG, String.valueOf("pos:"+col+","+row));
+            validPos.set(col, row+1);
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<ArrayList<Integer>> initBoardState(){
+        ArrayList<ArrayList<Integer>> tempBoard = new ArrayList<>();
+        for ( int col = 0; col < 7; col++){
+            ArrayList<Integer> tempCol = new ArrayList<>();
+            for (int row = 0; row < 6; row++){
+                tempCol.add(0);
+            }
+            tempBoard.add(tempCol);
+        }
+        Log.i(TAG, String.valueOf("boardState.size():"+tempBoard.size()));
+        Log.i(TAG, String.valueOf("boardState.get(0).size():"+tempBoard.get(0).size()));
+        return tempBoard;
+    }
+
+    public void constructBoardState(String color, int x, int y, int board){
         //column first
         int code = 0;
+        int posX = 5 - x;
+        int posY = y;
+        ArrayList<Integer> tempCol = new ArrayList<>();
         if(color == "G"){
             code = 1;
         }
         else if(color == "P"){
             code = 2;
+        }
+        if (board == 0){
+//            Log.i(TAG, String.valueOf("X:"+posX+", Y:"+posY+", code:"+code));
+            tempCol = preState.get(posY);
+            tempCol.set(posX, code);
+            preState.set(posY, tempCol);
+        }
+        else if(board == 1){
+            tempCol = currentState.get(posY);
+            tempCol.set(posX, code);
+            currentState.set(posY, tempCol);
+//            Log.i(TAG, String.valueOf("X:"+posX+", Y:"+posY+", code:"+currentState.get(posY).get(posX)));
         }
 
 
